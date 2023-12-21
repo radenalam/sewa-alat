3d73828974441b3161eebc36c8c8f1a6b4c27733
radenalam <radenalamsanjaya@gmail.com>
2023-12-21 07:55:54 +0700

    progres paga detail product

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,8 +5,19 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ProductProps } from "@/types/index";
 import Image from "next/image";
+import {
+  Box,
+  Button,
+  Dialog,
+  Flex,
+  Tabs,
+  TextArea,
+  TextField,
+} from "@radix-ui/themes";
+import { useForm } from "react-hook-form";
 
 const ProductDetails = ({ params }: { params: { id: string } }) => {
+  const { handleSubmit, register } = useForm<ProductProps>({});
   const [product, setProduct] = useState<ProductProps | null>(null);
 
   useEffect(() => {
@@ -17,24 +28,46 @@ const ProductDetails = ({ params }: { params: { id: string } }) => {
   }, [params.id]); // Include params.id as a dependency
 
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="mx-8">Product Details {params.id}</div>
-      {product && (
-        <div>
-          <div>Name: {product.name}</div>
-          <div>Description: {product.description}</div>
-          <div>Price: {product.price}</div>
-          <div>Image: {product.image}</div>
-          <Image
-            src={product.image ?? "/default_camera.png"}
-            alt={product.name}
-            width={500}
-            height={500}
-          />
+      <div className="flex mx-14 my-5 bg-gray-200 border px-2 py-2 rounded-xl">
+        <div className="w-1/2 bg-red-100 flex flex-col items-center">
+          {product && (
+            <div>
+              <Image
+                className=""
+                src={product.image ?? "/default_camera.png"}
+                alt={product.name}
+                width={400}
+                height={400}
+              />
+
+              <p>Name: {product.name}</p>
+              <p>Description: {product.description}</p>
+              <p>Price: {product.price}</p>
+              <p>Image: {product.image}</p>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+
+        <div className="w-1/2 bg-blue-100 flex flex-col justify-center items-center">
+          <div className="flex flex-row items-center">
+            <label className="mr-2">Nama</label>
+            <TextField.Input
+              placeholder="Nama"
+              {...register("name", { required: "Name is required" })}
+            />
+          </div>
+          <div className="flex flex-row items-center">
+            <label className="mr-2">Alamat</label>
+            <TextField.Input
+              placeholder="Alamat"
+              {...register("name", { required: "alamat is required" })}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
